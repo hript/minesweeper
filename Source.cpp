ef3e7868a8cb2e4e195584ebfd585f01957048b3
Artem Arhipov <artemiy3893@gmail.com>
2020-06-06 14:54:07 +0300

    show field

--- Source.cpp
+++ Source.cpp
@@ -8,9 +8,16 @@
 
 using namespace std;
 
-char field[SIZE_OF_FIELD][SIZE_OF_FIELD] = { '0' };
+char field[SIZE_OF_FIELD][SIZE_OF_FIELD];
 
-void landMines(int countOfMines){
+void initField() {
+	for (int i = 0; i < SIZE_OF_FIELD; i++) {
+		for (int j = 0; j < SIZE_OF_FIELD; j++) {
+			field[i][j] = '0';
+		}
+	}
+}
+void landMines(){
 	int pos_row, pos_column;
 	do {
 		 pos_row = rand() % SIZE_OF_FIELD;
@@ -20,12 +27,27 @@ void landMines(int countOfMines){
 	field[pos_row][pos_column] = MINE_CHAR;
 }
 
+void showField() {
+	for (int i = 0; i < SIZE_OF_FIELD; i++) {
+		for (int j = 0; j < SIZE_OF_FIELD; j++) {
+			cout << field[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
 int main()
 {
-	srand(time(0));// 2 2 2 2 2
+	srand(time(0));
+	
 	int countOfMines = SIZE_OF_FIELD * SIZE_OF_FIELD * PART_OF_FIELD;
-	
-	
+
+	initField();
+
+	for (int i = 0; i < countOfMines; i++) {
+		landMines();
+	}
+
+	showField();
 
 	return 0;
 }

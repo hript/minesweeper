1ef3a20df4bb2e23e7a153c45e80532e002004b6
Artem Arhipov <artemiy3893@gmail.com>
2020-06-06 16:03:53 +0300

    all

--- Source.cpp
+++ Source.cpp
@@ -14,10 +14,7 @@ char** fieldMine;
 char** fieldPlayer;
 
 void initField(char** field, char c) {
-	field = new char* [SIZE_OF_FIELD];
-
-	for (int i = 0; i < SIZE_OF_FIELD; i++)
-		field[i] = new char[SIZE_OF_FIELD];
+	
 
 	for (int i = 0; i < SIZE_OF_FIELD; i++) {
 		for (int j = 0; j < SIZE_OF_FIELD; j++) {
@@ -26,14 +23,14 @@ void initField(char** field, char c) {
 	}
 	
 }
-void landMines(){
+void landMines(char** field){
 	int pos_row, pos_column;
 	do {
 		 pos_row = rand() % SIZE_OF_FIELD;
 		 pos_column = rand() % SIZE_OF_FIELD;
 
-	} while (fieldMine[pos_row][pos_column] == MINE_CHAR);
-	fieldMine[pos_row][pos_column] = MINE_CHAR;
+	} while (field[pos_row][pos_column] == MINE_CHAR);
+	field[pos_row][pos_column] = MINE_CHAR;
 }
 
 void showField(char** field) {
@@ -60,8 +57,24 @@ bool openCell(int row, int column) {
 	if (fieldMine[row][column] == MINE_CHAR)
 		return false;
 
-	fieldPlayer[row][column] = '0' + countNeib(row, column);
+	int count = countNeib(row, column);
 
+	fieldPlayer[row][column] = '0' + count;
+	
+	if (count == 0) {
+		for (int i = row - 1; i <= row + 1; i++) {
+			for (int j = column - 1; j <= column + 1; j++) {
+				if (i >= 0 && j >= 0 && i < SIZE_OF_FIELD && j < SIZE_OF_FIELD) {
+					if (i == row && j == column)
+						continue;
+					if (fieldPlayer[i][j] == EMPTY)
+						openCell(i, j);
+
+
+				}
+			}
+		}
+	}
 	return true;
 }
 
@@ -71,21 +84,44 @@ int main()
 	
 	int countOfMines = SIZE_OF_FIELD * SIZE_OF_FIELD * PART_OF_FIELD;
 
+	fieldMine = new char* [SIZE_OF_FIELD];
+	for (int i = 0; i < SIZE_OF_FIELD; i++)
+		fieldMine[i] = new char[SIZE_OF_FIELD];
+
+	fieldPlayer = new char* [SIZE_OF_FIELD];
+	for (int i = 0; i < SIZE_OF_FIELD; i++)
+		fieldPlayer[i] = new char[SIZE_OF_FIELD];
+
 	initField(fieldMine, EMPTY);
 	initField(fieldPlayer, EMPTY);
 
 	for (int i = 0; i < countOfMines; i++) {
-		landMines();
+		landMines(fieldMine);
 	}
+
+	showField(fieldMine);
+	cout << endl;
 
 	int row, column;
 	cin >> row >> column;
+	row -= 1;
+	column -= 1;
 
 	if (fieldMine[row][column] == MINE_CHAR) {
 		fieldMine[row][column] = EMPTY;
 	}
 
-	//showField();
+	while (1) {
+		openCell(row , column);
+		//system("cls");
+		showField(fieldPlayer);
+		cin >> row >> column;
+		row -= 1;
+		column -= 1;
+		if (!openCell(row, column))
+			return 0;
+		
+	}
 
 	return 0;
 }
